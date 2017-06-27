f5243e4e5873b3f8d7e71121161239f7b511dc61
Frank Errickson <frankerrickson@berkeley.edu>
2017-06-27 07:58:59 -0700

    Add pair of brackets needed in welfare equation

--- src/components/nice_welfare_component.jl
+++ src/components/nice_welfare_component.jl
@@ -17,9 +17,9 @@ function run_timestep(state::nice_welfare, t::Int)
     v, p, d = getvpd(state)
 
     if t==1
-        v.welfare = sum((p.quintile_c[t,:,:] .^ (1.0 - p.eta)) ./ (1.0 - p.eta) .* p.pop[t,:,:]) / (1.0 + p.rho)^(10*t-1)
+        v.welfare = sum((p.quintile_c[t,:,:] .^ (1.0 - p.eta)) ./ (1.0 - p.eta) .* p.pop[t,:,:]) / (1.0 + p.rho)^(10*(t-1))
     else
-        v.welfare = v.welfare + sum((p.quintile_c[t,:,:] .^ (1.0 - p.eta)) ./ (1.0 - p.eta) .* p.pop[t,:,:]) / (1.0 + p.rho)^(10*t-1)
+        v.welfare = v.welfare + sum((p.quintile_c[t,:,:] .^ (1.0 - p.eta)) ./ (1.0 - p.eta) .* p.pop[t,:,:]) / (1.0 + p.rho)^(10*(t-1))
     end
 
 end
