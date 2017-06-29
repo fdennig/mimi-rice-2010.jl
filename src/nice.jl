6d786ef8ea4b70f572ffb6e18dab8a0dda3f95e7
Frank Errickson <frankerrickson@berkeley.edu>
2017-06-29 13:17:15 -0700

    Modify construct NICE function to account for custom input type

--- src/nice.jl
+++ src/nice.jl
@@ -6,22 +6,21 @@ include(joinpath(dirname(@__FILE__), "components/nice_components/nice_welfare_co
 include(joinpath(dirname(@__FILE__), "rice2010.jl"))
 include(joinpath(dirname(@__FILE__), "helpers.jl"))
 
-function construct_nice(nsteps=60, xi=1.0, omega=1.0)
-
+function construct_nice(inputs::NICE_inputs)
 
     # Read in income quintile distribution data
     income_dist = Array(readtable(joinpath(dirname(@__FILE__), "../data/income_quintiles_RICE.csv"), header=false))
 
     # Estimate damage and abatement distributions for quintiles
-    damage_dist = quintile_dist(xi, income_dist)
-    abatement_dist = quintile_dist(omega, income_dist)
+    damage_dist = quintile_dist(inputs.xi, income_dist)
+    abatement_dist = quintile_dist(inputs.omega, income_dist)
 
     # Construct RICE2010 and load RICE parameters
     m, rice_params = getrice()
 
     # Create variable for income quintile populations for each region.
     rice_population = rice_params[:l]
-    pop_data = 0.2 .* reshape(repmat(rice_population,1,5), nsteps, 12, 5)
+    pop_data = 0.2 .* reshape(repmat(rice_population,1,5), inputs.nsteps, 12, 5)
 
     # Set quintile index
     setindex(m, :quintiles, ["First", "Second", "Third", "Fourth", "Fifth"])
@@ -34,22 +33,24 @@ function construct_nice(nsteps=60, xi=1.0, omega=1.0)
     addcomponent(m, nice_welfare, after=:nice_consumption)
 
     # Set all model parameters
-    setparameter(m, :grosseconomy, :dk, ones(12))
+    setparameter(m, :grosseconomy, :dk, inputs.cap_dep)
+
+    setparameter(m, :emissions, :MIU, inputs.abate)
 
     setparameter(m, :nice_consumption, :income_dist, transpose(income_dist ./ 100))
     setparameter(m, :nice_consumption, :damage_dist, transpose(damage_dist ./ 100))
     setparameter(m, :nice_consumption, :abatement_dist, transpose(abatement_dist ./ 100))
 
     setparameter(m, :nice_welfare, :pop, pop_data)
-    setparameter(m, :nice_welfare, :rho, 0.015)
-    setparameter(m, :nice_welfare, :eta, 1.5)
+    setparameter(m, :nice_welfare, :rho, inputs.rho)
+    setparameter(m, :nice_welfare, :eta, inputs.eta)
 
     # Set new savings rate (base NICE value differs from RICE savings rate)
-    setparameter(m, :neteconomy, :S, (ones(nsteps,12) .* 0.2585))
+    setparameter(m, :neteconomy, :S, (ones(inputs.nsteps,12) .* 0.2585))
 
     # Make model component connections.
     connectparameter(m, :nice_consumption, :CPC, :neteconomy, :CPC)
-    connectparameter(m, :nice_consumption, :DAMFRAC, :damages, :DAMFRAC)
+    connectparameter(m, :nice_consumption, :DAMFRAC, :damages, :DAMFRAC )
     connectparameter(m, :nice_consumption, :ABATEFRAC, :emissions, :ABATEFRAC)
     connectparameter(m, :nice_welfare, :quintile_c, :nice_consumption, :quintile_c)
 
@@ -58,10 +59,10 @@ end
 
 
 # Create NICE objective function, passing in version of NICE made with "construct_nice()" function.
-function construct_nice_objective()
+function construct_nice_objective(inputs::NICE_inputs)
 
     # Get an implementation of the NICE model
-    m, rice_params = construct_nice()
+    m, rice_params = construct_nice(inputs)
 
     # Get backstop prices from base version of RICE
     rice_backstop = rice_params[:pbacktime]
@@ -69,12 +70,12 @@ function construct_nice_objective()
     function nice_objective(tax::Array{Float64,1})
 
         # Calculate emissions abatement level as a function of the carbon tax.
-        abatement_level = mu_from_tax(tax, rice_backstop, 2.8)
+        abatement_level, tax = mu_from_tax(tax, rice_backstop, 2.8)
 
         setparameter(m, :emissions, :MIU, abatement_level)
         run(m)
         return m[:nice_welfare, :welfare]
     end
 
-    return nice_objective, rice_params
+    return nice_objective, m, rice_params
 end
